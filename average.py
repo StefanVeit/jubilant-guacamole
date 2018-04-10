d100f49c0371942c77fe03af4028162fcb1c1f2c
StefanVeit <coconut1710@gmx.net>
2018-04-10 09:50:20 +0200

    Initial commit

---
+++ average.py
@@ -0,0 +1,13 @@
+import random
+
+n = 100000000
+b = []
+for i in range(1,n):
+    a = random.randint(1,6)
+    b.append(a)
+
+c = sum(b)
+
+average = c/n
+
+print('Durschnittliche Würfelzahl:',average)
